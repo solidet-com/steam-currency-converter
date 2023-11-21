08741e69ffe088d9064f5d1f6eceb907a81dde3b
Fudayl Cavus <fudaylcavus@gmail.com>
2023-11-22 01:18:35 +0300

    fix: dlc-list item on discount

--- utils/globals.js
+++ utils/globals.js
@@ -10,15 +10,19 @@ const DISCOUNT_PRICE = ".discount_prices > *";
 const CART_ITEM_PRICE = ".cart_item_price .price";
 const SEARCH_ITEM_PRICE = ".match_subtitle";
 const TOTAL_CART_PRICE = "#cart_estimated_total";
-const DLC_LIST_PRICE = ".game_area_dlc_price";
+const DLC_LIST_PRICE = ".game_area_dlc_price:not(:has(.discount_prices))";
 
-/* -- TOP_SELLER SELECTORS -- */ 
+/* -- TOP_SELLER SELECTORS -- */
 const TOP_SELLERS_PAGE_DISC_PRICE = ".salepreviewwidgets_StoreSaleDiscountedPriceCtn_3GLeQ > *";
 const TOP_SELLERS_PAGE_PRICE = ".salepreviewwidgets_StoreSalePriceCtn_3R7Q6 > *";
 const GAME_WITH_EVENT_PRICE = ".eventrow_AppCapsulePrice_Jq75m > *";
 const DLC_DISCOUNT_PRICE = ".salepreviewwidgets_StoreSaleDiscountedPriceCtn_3GLeQ > *";
 const DLC_PRICE = ".salepreviewwidgets_StoreSalePriceBox_Wh0L8";
 
+/* -- BUNDLE SELECTORS -- */
+const BUNDLE_PACKAGE_PRICE = ".bundle_final_package_price";
+const BUNDLE_PCK_DISCOUNT_PRICE = ".bundle_final_price_with_discount";
+const BUNDLE_SAVINGS_PRICE = '.bundle_savings'
 
 const COMMON_SELECTORS = [
     DISCOUNT_PRICE,
@@ -30,5 +34,9 @@ const COMMON_SELECTORS = [
     GAME_WITH_EVENT_PRICE,
     DLC_LIST_PRICE,
     DLC_PRICE,
+
+    BUNDLE_PACKAGE_PRICE,
+    BUNDLE_PCK_DISCOUNT_PRICE,
+    BUNDLE_SAVINGS_PRICE
 ];
 let exchangeRatePromise;
